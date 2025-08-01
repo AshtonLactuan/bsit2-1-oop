32bfcf3eea6111db151f4e187c809b6f7b3575e6
Ashton Lactuan <158186830+AshtonLactuan@users.noreply.github.com>
2025-08-01 11:34:32 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int totalNumber = 0;
 
         for (int i = 1; i <= 5; i++) {
             System.out.print("Enter number " + i + ": ");
-            int number = input.nextInt();
+            int number = scanner.nextInt();
             totalNumber += number;
         }
 
         System.out.println("\nTotal Number: " + totalNumber);
-        input.close();
+        scanner.close();
     }
 }
