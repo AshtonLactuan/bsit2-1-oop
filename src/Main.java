77708565b15c2df4c686b6d27d43c30a83293b64
Ashton Nathaniel Lactuan <anlactuan93824@liceo.edu.ph>
2025-08-18 10:02:19 +0800

    lab-activity-4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+        String student = "John Smith";
+        double avg = new GradeCalculator().calculateAverage(85.5, 92.0, 78.5, 90.0);
+        String grade = new GradeCalculator().getLetterGrade(avg);
+
+        new GradeCalculator().displayResult(student, avg);
+        new GradeCalculator().displayResult(student, avg, grade);
     }
 }
