ba5143ea8e69979fcc223e808a91fbb3491483c3
Ashton Lactuan <158186830+AshtonLactuan@users.noreply.github.com>
2025-09-02 20:56:00 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Library Management System");
-        LibraryManager.showBooks();
-        LibraryManager.addBook();
-        LibraryManager.removeBook();
-        System.out.println("\nProgram completed successfully.");
+        LibraryManager manager = new LibraryManager();
+
+        manager.showBooks();
+        manager.addBook();
+        manager.removeBook();
+
+        System.out.println("Program completed successfully.");
     }
 }
