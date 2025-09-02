b4e46cd4c0b83ef459b74559b8063bbe232bd975
Ashton Lactuan <158186830+AshtonLactuan@users.noreply.github.com>
2025-09-02 20:32:32 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+        System.out.println("Library Management System");
+        LibraryManager.showBooks();
+        LibraryManager.addBook();
+        LibraryManager.removeBook();
+        System.out.println("\nProgram completed successfully.");
     }
 }
