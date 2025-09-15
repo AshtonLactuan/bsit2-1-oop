2ffe6dd19d40562934025d576104f53d7d90e17d
Ashton Lactuan <158186830+AshtonLactuan@users.noreply.github.com>
2025-09-15 13:35:28 +0800

    Update Pet.java

--- src/Pet.java
+++ src/Pet.java
@@ -1,1 +1,61 @@
+interface Trainable {
+    void performTrick();
+}
+
+abstract class Pet {
+    protected String name;
+    protected int age;
+
+    public Pet(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public abstract void makeSound();
+
+    public void displayInfo() {
+        System.out.println("Pet Name: " + name + ", Age: " + age);
+        System.out.print(name + " says: ");
+        makeSound();
+        System.out.println();
+    }
+}
+
+class Dog extends Pet implements Trainable {
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    public void makeSound() {
+        System.out.println("Woof! Woof!");
+    }
+
+    public void performTrick() {
+        System.out.println("Training " + name + ": Sits and shakes hands");
+    }
+}
+
+class Cat extends Pet {
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    public void makeSound() {
+        System.out.println("Meow! Meow!");
+    }
+}
+
+class Bird extends Pet implements Trainable {
+    public Bird(String name, int age) {
+        super(name, age);
+    }
+
+    public void makeSound() {
+        System.out.println("Tweet! Tweet!");
+    }
+
+    public void performTrick() {
+        System.out.println("Training " + name + ": Flies in circles and lands on perch");
+    }
+}
 
